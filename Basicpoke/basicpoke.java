c76f35f7d361faedbb00d48ded429dcacadd2e01
juloung123 <jokergamer123@hotmail.com>
2020-03-31 01:40:32 +0700

    commite

--- Basicpoke/basicpoke.java
+++ Basicpoke/basicpoke.java
@@ -1,11 +1,99 @@
 package Basicpoke;
+import java.util.*;
 
 public class basicpoke{
-    public int hp = 0;
-    public int sp = 0;
-    public int atk = 0;
-    public int def = 0;
-    public int skill1 = 0;
-    public int skill2 = 0;
-    public int level = 0;
+    public int hp;
+    public int sp;
+    public int atk;
+    public int def;
+    public int skill;
+    public int level;
+    public int exp;
+    public String Race;
+    public int MaxHp;
+
+    public basicpoke(){
+        Race = "Nothing";
+        hp = 1;
+        sp = 1;
+        atk = 1;
+        def = 1;
+        skill = 1;
+        level = 1;
+        exp = 0;
+        MaxHp = 1;
+    }
+    public void status(String name){
+        System.out.println("Name :"+name);
+            System.out.println("Race :" + Race);
+            System.out.println("Level :"+getLv());
+            System.out.println("Exp :"+getExp());
+            System.out.println("Hp :" + getHp() + "/" + getMaxHp());
+            System.out.println("SP :"+ getSp());
+            System.out.println("Defence :" + getdef());
+            System.out.println("Attack Damage " + getAttackDamage() + "\nSkill Damage"+ getMagicDamage());
+    }
+    public double getAttackDamage(){
+        return atk;
+    }
+    public double getMagicDamage(){
+        return skill;
+    }
+    public int getLv(){
+        return level;
+    }
+    public double getExp(){
+        return exp;
+    }
+    public int getHp(){
+        return hp;
+    }
+    public int getSp(){
+        return sp;
+    }
+    public int getMaxHp(){
+        return MaxHp;
+    }
+    public int getdef(){
+        return def;
+    }
+    public void setExp(int earn){
+            exp = exp + earn;
+        }
+    public void regenHp(int maxHp){
+        hp = maxHp;
+        sp = maxHp;
+        System.out.println("Your pokemon HP and SP Will be Regen");
+    }
+    public void setDamage(int damage){
+        hp = hp-damage;
+        if(hp <= 0 ){
+            System.out.println("YOU Pokemon DIE !");
+            System.out.println("Please Go to pokemon center!!");
+        }
+    }
+    public void setSp(int decrease){
+        sp = sp - decrease;
+    }
+    public void hitMonster(Bag bag){
+        if(level == 1 && exp == 0){
+            bag.addItem();
+            System.out.println("Congratulations you get  5 of hp Potion 5 of Sp Potion 5 Pokeball and 5 Berries");
+        }
+        setExp(150);
+        setDamage(50);
+        setSp(10);
+        setLv(exp);
+
+        Random rand = new Random();
+        int value = rand.nextInt(5);  // Can get Red Potion if random is 0 and Blue Potion is 1 another don't get anything
+        if (value == 0){
+            bag.Red();
+            System.out.println("You get Red Potion 1");
+        }
+        else if(value == 1){
+            bag.Blue();
+            System.out.println("You get Blue Potion 1");
+        }
+    }
 }
